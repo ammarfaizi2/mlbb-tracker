3dba1c941d21d897b274c06c36e7d8dfbc05653d
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2024-08-03 13:52:58 +0700

    fix: font family in tailwind config
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,28 +59,26 @@ const config: Config = {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)'
       },
-      extend: {
-        fontFamily: {
-          // BEAUFORT FONTS
-          'beaufort-light': ['Beaufort-Light', ...theme.fontFamily.sans],
-          'beaufort-lightitalic': ['Beaufort-LightItalic', ...theme.fontFamily.sans],
-          'beaufort-regular': ['Beaufort-Regular', ...theme.fontFamily.sans],
-          'beaufort-italic': ['Beaufort-Italic', ...theme.fontFamily.sans],
-          'beaufort-medium': ['Beaufort-Medium', ...theme.fontFamily.sans],
-          'beaufort-mediumitalic': ['Beaufort-MediumItalic', ...theme.fontFamily.sans],
-          'beaufort-bold': ['Beaufort-Bold', ...theme.fontFamily.sans],
-          'beaufort-bolditalic': ['Beaufort-BoldItalic', ...theme.fontFamily.sans],
-          'beaufort-heavy': ['Beaufort-Heavy', ...theme.fontFamily.sans],
-          'beaufort-heavyitalic': ['Beaufort-HeavyItalic', ...theme.fontFamily.sans],
+      fontFamily: {
+        // BEAUFORT FONTS
+        'beaufort-light': ['Beaufort-Light', ...theme.fontFamily.sans],
+        'beaufort-lightitalic': ['Beaufort-LightItalic', ...theme.fontFamily.sans],
+        'beaufort-regular': ['Beaufort-Regular', ...theme.fontFamily.sans],
+        'beaufort-italic': ['Beaufort-Italic', ...theme.fontFamily.sans],
+        'beaufort-medium': ['Beaufort-Medium', ...theme.fontFamily.sans],
+        'beaufort-mediumitalic': ['Beaufort-MediumItalic', ...theme.fontFamily.sans],
+        'beaufort-bold': ['Beaufort-Bold', ...theme.fontFamily.sans],
+        'beaufort-bolditalic': ['Beaufort-BoldItalic', ...theme.fontFamily.sans],
+        'beaufort-heavy': ['Beaufort-Heavy', ...theme.fontFamily.sans],
+        'beaufort-heavyitalic': ['Beaufort-HeavyItalic', ...theme.fontFamily.sans],
 
-          // SPIEGEL FONTS
-          'spiegel-regular': ['Spiegel-Regular', ...theme.fontFamily.sans],
-          'spiegel-regularitalic': ['Spiegel-RegularItalic', ...theme.fontFamily.sans],
-          'spiegel-semiBold': ['Spiegel-SemiBold', ...theme.fontFamily.sans],
-          'spiegel-semiBolditalic': ['Spiegel-SemiBoldItalic', ...theme.fontFamily.sans],
-          'spiegel-bold': ['Spiegel-Bold', ...theme.fontFamily.sans],
-          'spiegel-bolditalic': ['Spiegel-BoldItalic', ...theme.fontFamily.sans]
-        }
+        // SPIEGEL FONTS
+        'spiegel-regular': ['Spiegel-Regular', ...theme.fontFamily.sans],
+        'spiegel-regularitalic': ['Spiegel-RegularItalic', ...theme.fontFamily.sans],
+        'spiegel-semiBold': ['Spiegel-SemiBold', ...theme.fontFamily.sans],
+        'spiegel-semiBolditalic': ['Spiegel-SemiBoldItalic', ...theme.fontFamily.sans],
+        'spiegel-bold': ['Spiegel-Bold', ...theme.fontFamily.sans],
+        'spiegel-bolditalic': ['Spiegel-BoldItalic', ...theme.fontFamily.sans]
       }
     }
   }
