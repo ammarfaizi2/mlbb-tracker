188d6c4e96893a9ab28118e33c33cb6511ad286c
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2024-08-03 12:37:40 +0700

    feat: add background image
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,15 @@
   import '../font.css';
 </script>
 
-<slot />
+<main class="relative min-h-screen w-full">
+  <div>
+    <img
+      src="/images/background.webp"
+      alt="Site Background"
+      class="h-screen w-screen object-cover brightness-[30%] blur-[2px]"
+    />
+  </div>
+  <div class="absolute left-0 top-0 min-h-screen w-full bg-black/50 text-white px-10 py-8">
+    <slot />
+  </div>
+</main>
