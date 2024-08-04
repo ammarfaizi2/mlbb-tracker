a467f19b7aec956584b5d98dbba93dea782d75ba
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2024-08-05 01:52:50 +0700

    refactor(root): change some css styling
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,11 +9,11 @@
     alt="Site Background"
     class="h-screen w-screen object-cover brightness-[30%]"
   />
-  <div class="absolute left-0 top-0 z-[1] h-screen w-screen object-cover backdrop-blur-[.1vw]" />
+  <div class="absolute left-0 top-0 h-screen w-screen object-cover backdrop-blur-[.1vw]" />
   <div
-    class="absolute left-0 top-0 z-[2] min-h-screen w-full bg-black/70 font-spiegel-regular text-white"
+    class="absolute left-0 top-0 min-h-screen w-full bg-black/70 font-spiegel-regular text-white"
   >
-    <div class="container px-10 py-8">
+    <div class="container h-screen px-10 py-8">
       <slot />
     </div>
   </div>
